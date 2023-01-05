4f6df456ccfc07a8ab670374ab9e342b51771fa0
sargoschh <sargosch@tsn.at>
2023-01-05 16:51:56 +0100

    SpaßMitSpríngBoot implementation begonnen

---
+++ studentenverwaltung/src/main/java/at/itkolleg/studentenverwaltung/StudentenverwaltungApplication.java
@@ -0,0 +1,13 @@
+package at.itkolleg.studentenverwaltung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentenverwaltungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentenverwaltungApplication.class, args);
+	}
+
+}
---
+++ studentenverwaltung/src/test/java/at/itkolleg/studentenverwaltung/StudentenverwaltungApplicationTests.java
@@ -0,0 +1,13 @@
+package at.itkolleg.studentenverwaltung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentenverwaltungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

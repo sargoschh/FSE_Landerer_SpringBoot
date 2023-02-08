3160a30cde03dc8ec1476ab7fbf95f041b292597
sargosch <sargosch@tsn.at>
2023-02-08 08:14:16 +0100

    GrowManager Projekt eingerichtet

---
+++ GrowManager/src/main/java/at/itkolleg/growmanager/GrowManagerApplication.java
@@ -0,0 +1,13 @@
+package at.itkolleg.growmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrowManagerApplication.class, args);
+    }
+
+}
